97f5788a662b808debce04a1f8eecb4a05497f21
Christopher Lee <hanernlee@gmail.com>
2018-08-11 14:23:21 +1000

    Completing builder pattern

---
+++ Chapter9-BuilderPattern/RabbleWabble/RabbleWabble/Builder/QuestionGroupBuilder.swift
@@ -0,0 +1,52 @@
+  //
+//  QuestionGroupBuilder.swift
+//  RabbleWabble
+//
+//  Created by Christopher Lee on 11/8/18.
+//  Copyright © 2018 Razeware, LLC. All rights reserved.
+//
+
+import Foundation
+
+public class QuestionBuilder {
+  public var answer = ""
+  public var hint = ""
+  public var prompt = ""
+  
+  public func build() throws -> Question {
+    guard answer.count > 0 else { throw Error.missingAnswer }
+    guard prompt.count > 0 else { throw Error.missingPrompt }
+    return Question(answer: answer, hint: hint, prompt: prompt)
+  }
+  
+  public enum Error: String, Swift.Error {
+    case missingAnswer
+    case missingPrompt
+  }
+}
+
+public class QuestionGroupBuilder {
+  public var questions = [QuestionBuilder()]
+  public var title = ""
+  
+  public func addNewQuestions() {
+    let question = QuestionBuilder()
+    questions.append(question)
+  }
+  
+  public func removeQuestion(at index: Int) {
+    questions.remove(at: index)
+  }
+  
+  public func build() throws -> QuestionGroup {
+    guard self.title.count > 0 else { throw Error.missingTitle }
+    guard self.questions.count > 0 else { throw Error.missingQuestions }
+    let questions = try self.questions.map { try $0.build() }
+    return QuestionGroup(questions: questions, title: title)
+  }
+  
+  public enum Error: String, Swift.Error {
+    case missingTitle
+    case missingQuestions
+  }
+}
